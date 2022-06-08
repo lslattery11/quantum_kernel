6e5d31db52c727533a94e5b98b5eed924cd23f00
lslattery11 <45021369+lslattery11@users.noreply.github.com>
2022-06-08 09:31:35 -0500

    parallel bash script for multiscale featuremap test

--- scripts/multi_compute_qk_HZZmulti.sh
+++ scripts/multi_compute_qk_HZZmulti.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 export PYTHONPATH=${PYTHONPATH}:/mnt/c/Users/lslat/Code/
 export PYTHONPATH=${PYTHONPATH}:/mnt/c/Users/lslat/QiskitProjects/VariationalWavefunction/
+export PYTHONPATH=${PYTHONPATH}:/media/HomeData/lslattery/
 
-for i in 0 0.1 1 2
-do
-for j in 0 0.001 0.1 0.2 0.5 1.0
-do
-python compute_quantum_kernel_HZZMulti.py --outpath /mnt/c/Users/lslat/Data/QK_project/results/hproj_HZZMulti/fashion-mnist/ --dataset-dim 10 \
---n-trotter 10 \
---evo-time 1 \
---init-state 'Haar_random' \
---init-state-seed 0 \
---lam1 $i \
---lam2 $j \
---dataset fashion-mnist \
---projected huang_proj
-done
-done
+data_dim=(2 4 6 8 10 12)
+lam1=(0 0.01 0.1 1.0)
+lam2=(0 0.01 0.1 1.0)
+
+parallel \
+    --jobs 20 \
+    """
+        python compute_quantum_kernel_HZZMulti.py --outpath media/HomeData/lslattery/QK_project/results/hproj_HZZMulti/fashion-mnist/ --dataset-dim {1} \
+        --n-trotter 10 \
+        --evo-time 1 \
+        --init-state 'Haar_random' \
+        --init-state-seed 0 \
+        --lam1 {2} \
+        --lam2 {3} \
+        --dataset fashion-mnist \
+        --projected huang_proj
+    """ ::: "${data_dim[@]}" ::: "${lam1[@]}" ::: "${lam2[@]}"
