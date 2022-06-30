9f6f08c5ed33260c1f6ca12811d0278b9109c7f6
lslattery11 <lslatte2@gmail.com>
2022-06-30 12:50:41 -0500

    testing for this script

--- scripts/compute_Sparse_IQP_kernel_gennorm_data.py
+++ scripts/compute_Sparse_IQP_kernel_gennorm_data.py
@@ -57,8 +57,13 @@ if __name__ == '__main__':
         proj='_'+args.projected
     else:
         proj=args.projected
-
+    print(args.outpath)
+    print(args.dataset_dim)
+    print(scaling_factor)
+    print(int_time_scale)
+    print(args.density)
     outpath = Path(args.outpath, f"Sparse_IQP_dim_{args.dataset_dim}{proj}_scales_{scaling_factor}_{int_time_scale}_density_{args.density}.p")
+    
     if outpath.exists():
         print(f"Found already computed at {outpath}, exiting")
         sys.exit()
