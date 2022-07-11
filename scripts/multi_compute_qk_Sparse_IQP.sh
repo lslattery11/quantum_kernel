1729765f03257c0befe0af744f89744242bef272
lslattery11 <lslatte2@illinois.edu>
2022-07-11 09:23:49 -0500

    update for multi compute

--- scripts/multi_compute_qk_Sparse_IQP.sh
+++ scripts/multi_compute_qk_Sparse_IQP.sh
@@ -27,7 +27,7 @@ beta=(0.1 1.0 2.0)
 parallel \
     --jobs 14 \
     """
-        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/gamma0.2/ \
+        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/ \
         --dataset-dim {2} \
         --scaling-factor {1} \
         --int-scaling-factor 1.0 \
@@ -39,14 +39,14 @@ parallel \
 #calculated from q_curve interpolation
 sf=(0.6745147380670223 0.5684518372842262 0.49476028790019533 0.44998769698234725 0.4052151060644992 0.3813693660751997 0.36028254130998344 0.3391957165447671 0.3181088917795508 0.29770041798863284 0.2814170149905274 0.2651336119924219 0.24885020899431642 0.23256680599621096])
 dim=(5 6 7 8 9 10 11 12 13 14 15 16 17 18)
-parallel --link\
+parallel --link \
     --jobs 14 \
     """
-        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/gamma0.2 \
+        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/gamma0.2/ \
         --dataset-dim {2} \
         --scaling-factor {1} \
         --int-scaling-factor 1.0 \
         --density {2} \
-        --beta {3} \
+        --beta 1.0 \
         --projected ''
     """ ::: "${sf[@]}" ::: "${dim[@]}"
