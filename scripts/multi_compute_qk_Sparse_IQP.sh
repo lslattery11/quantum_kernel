d1ea15e1edff70a5320ebd78026730f5d64329d3
lslattery11 <lslatte2@illinois.edu>
2022-07-11 17:29:11 -0500

    edit to multi compute

--- scripts/multi_compute_qk_Sparse_IQP.sh
+++ scripts/multi_compute_qk_Sparse_IQP.sh
@@ -23,17 +23,18 @@ mapfile -t sf < logspace-2.1.30.txt
 dim=(4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 beta=(0.1 1.0 2.0)
 seed=(10 1200 33 4 210)
-# parallel \
-#     --jobs 14 \
-#     """
-#         python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/gamma0.2/ \
-#         --dataset-dim {2} \
-#         --scaling-factor {1} \
-#         --int-scaling-factor 1.0 \
-#         --density 0 \
-#         --beta {3} \
-#         --projected ''
-#     """ ::: "${sf[@]}" ::: "${dim[@]}" ::: "${beta[@]}"
+parallel \
+    --jobs 14 \
+    """
+        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/gamma0.2/ \
+        --dataset-dim {2} \
+        --scaling-factor {1} \
+        --int-scaling-factor 1.0 \
+        --density 0 \
+        --beta {3} \
+        --seed {4} \
+        --projected ''
+    """ ::: "${sf[@]}" ::: "${dim[@]}" ::: "${beta[@]}" ::: "${seed[@]}"
 
 #calculated from q_curve interpolation
 parallel \
