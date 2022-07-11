09c40a55a7dc1a5170f63ebaa2a3c67785f670d6
lslattery11 <lslatte2@illinois.edu>
2022-07-11 10:24:43 -0500

    update to multi compute

--- scripts/multi_compute_qk_Sparse_IQP.sh
+++ scripts/multi_compute_qk_Sparse_IQP.sh
@@ -22,12 +22,12 @@ export PYTHONPATH=${PYTHONPATH}:/home/lslattery/
 sf=(0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9)
 dim=(5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
 #beta=(0.05 0.1 0.2 0.5 1.0 5.0)
-beta=(0.1 1.0 2.0)
+beta=(1.0)
 
 parallel \
     --jobs 14 \
     """
-        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/ \
+        python compute_Sparse_IQP_kernel_gennorm_data.py --outpath ~/QK_project/results/Sparse_IQP/gennorm/gamma0.2/ \
         --dataset-dim {2} \
         --scaling-factor {1} \
         --int-scaling-factor 1.0 \
