e3eb24ea2beb7dceb55b0a0bd0944bbcbcf004bf
lslattery11 <lslatte2@illinois.edu>
2022-07-24 11:13:22 -0500

    update

--- code/feature_maps/HeisenbergZZMultiScale.py
+++ code/feature_maps/HeisenbergZZMultiScale.py
@@ -72,7 +72,7 @@ class HZZMultiscaleFeatureMap(BlueprintCircuit):
             #ZZ data layer
             for q1 in range(self._feature_dimension):
                 for q2 in range(q1 + 1, self._feature_dimension):
-                    phase=(np.pi-self.lam1*params[q1])*(np.pi-self.lam1*params[q2])
+                    phase=2*(np.pi-self.lam1*params[q1])*(np.pi-self.lam1*params[q2])
                     #qc.h(qr[q1])
                     #qc.h(qr[q2])
                     qc.cx(qr[q1],qr[q2])
