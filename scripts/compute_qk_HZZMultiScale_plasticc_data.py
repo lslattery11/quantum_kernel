5d691898217db9b451c94b54d872fb33ea096661
lslattery11 <lslatte2@gmail.com>
2022-08-12 11:50:33 -0500

    addeed compute for plasticc data

--- scripts/compute_qk_HZZMultiScale_plasticc_data.py
+++ scripts/compute_qk_HZZMultiScale_plasticc_data.py
@@ -11,8 +11,9 @@ import numpy as np
 import pickle
 from pathlib import Path
 from sklearn.svm import SVC
+from sklearn.metrics import balanced_accuracy_score
 
-from quantum_kernel.code.utils import get_dataset,get_quantum_kernel
+from quantum_kernel.code.utils import get_dataset,get_quantum_kernel,precomputed_kernel_GridSearchCV
 from quantum_kernel.code.feature_maps import HeisenbergZZMultiScale
 
 if __name__ == '__main__':
@@ -61,10 +62,6 @@ if __name__ == '__main__':
     int_scaling_factor=int_scaling_factor**(args.alpha/2)
 
     h_layer=args.h_layer
-    if args.projected != '':
-        proj='_'+args.projected
-    else:
-        proj=args.projected
 
     outpath = Path(args.outpath, f"HZZ_Multi_dim_{args.dataset_dim}_scales_{scaling_factor}_{int_scaling_factor}_{non_data_int_scaling_factor}_hlayer_{h_layer}_alpha_{args.alpha}.p")
 
@@ -78,7 +75,7 @@ if __name__ == '__main__':
     if args.dataset_dim > 30:
         raise ValueError(f"Dataset dimension {args.dataset_dim} too large; can support no more than 30 qubits")
 
-    x_train, x_test, y_train, y_test = get_dataset(args.dataset, args.dataset_dim, 800, 200)
+    x_train, x_test, y_train, y_test = get_dataset("plasticc", args.dataset_dim, 800, 200)
 
 
 
@@ -98,20 +95,36 @@ if __name__ == '__main__':
 
     qkern_matrix_test = qkern.evaluate(x_vec=x_test, y_vec=x_train)
 
+    t1 = time.time()
     K_test_time = t1-t0
     print(f"Done computing K_test in {K_test_time}")
-    t1 = time.time()
 
     qsvc = SVC(kernel='precomputed')
     qsvc.fit(qkern_matrix_train, y_train)
     score = qsvc.score(qkern_matrix_test, y_test)
     print(f"Score: {score}")
 
+    Cs=[0.006, 0.015, 0.03, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024]
+    best_C,best_g=precomputed_kernel_GridSearchCV(qkern_matrix_train,y_train,Cs,[0])
+
+    qsvc = SVC(kernel='precomputed', C=best_C)
+    qsvc.fit(qkern_matrix_train, y_train)
+    y_pred_train = qsvc.predict(qkern_matrix_train)
+
+    train_score=balanced_accuracy_score(y_train,y_pred_train)
+
+    y_pred_test = qsvc.predict(qkern_matrix_test)
+    test_score = balanced_accuracy_score(y_test,y_pred_test)
+
+    print(f"Optimized Train Score: {train_score}, and test score: {test_score}")
+
     res = {
             'qkern_matrix_train' : qkern_matrix_train,
             'qkern_matrix_test' : qkern_matrix_test,
             'args' : args,
             'K_train_time' : K_train_time,
+            'tuned_train_score' : train_score,
+            'tuned_test_score' : test_score,
     }
 
     pickle.dump(res, open(outpath, 'wb'))
--- scripts/multi_compute_qk_HZZmulti.sh
+++ scripts/multi_compute_qk_HZZmulti.sh
@@ -5,14 +5,14 @@ export PYTHONPATH=${PYTHONPATH}:/media/HomeData/lslattery/
 export PYTHONPATH=${PYTHONPATH}:/home/lslattery/
 
 
-mapfile -t sf < logspace-1.5.0.5.20.txt
-dim=(4 5 6 7 8 9 10 11 12 13 14 15 16,17,18)
+sf=(0.0316 0.0356 0.04 0.0451 0.0507 0.0571 0.0642 0.0723 0.0813 0.0915 0.103 0.1159 0.1304 0.1468 0.1652 0.1859 0.2092 0.2354 0.2649 0.2981 0.3355 0.3775 0.4248 0.4781 0.538 0.6054 0.6813 0.7667 0.8628 0.9709 1.0926 1.2295 1.3836 1.5571 1.7522 1.9718 2.219 2.4971 2.8101 3.1623)
+dim=(4 5 6 7 8 9 10 11 12 13 14 15 16 17 18)
 h=(0 1)
 
 parallel \
-    --jobs 30 \
+    --jobs 10 \
     """
-        python compute_HZZMultiScale_plasticc_data.py --outpath /media/HomeData/lslattery/QK_project/results/HZZ_multi/plasticc/ \
+        python compute_qk_HZZMultiScale_plasticc_data.py --outpath /media/HomeData/lslattery/QK_project/results/HZZ_multi/plasticc/ \
         --dataset-dim {2} \
         --scaling-factor {1} \
         --int-scaling-factor {1} \
